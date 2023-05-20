f0f04fc43f091a0c0a5ffe943656c05b8717b5c1
root <root@DESKTOP-M7QTL01>
2023-05-20 13:48:00 -0300

    Finishing lab006b

--- lab006b/mochila.c
+++ lab006b/mochila.c
@@ -1,38 +1,56 @@
-#include<stdio.h>
+#include <stdio.h>
+
+#define MAX_N 20
 
 typedef struct {
-    int peso,valor;
-}item;
+    int peso, valor, indice;
+} Objeto;
 
-int f(int quant_conjuntos, int peso_max, item conjunto[]);
+int melhor_valor = 0;
+int melhor_peso = 0;
+int tamanho_melhor_subconjunto = 0;
+int melhores_indices[MAX_N];
 
-int main(){
-    int quant_conjuntos, peso_max;
-    scanf("%d %d",&quant_conjuntos,&peso_max);
+void encontrar_subconjunto(Objeto S[], int n, int M, int mascara, int peso_subconjunto, int valor_subconjunto, int tamanho_subconjunto, int indices_subconjunto[]) {
+    if (peso_subconjunto > M) return;
 
-    item conjunto[quant_conjuntos];
-
-    for(int i=0;i<quant_conjuntos;++i){
-        scanf("%d %d",&conjunto[i].peso,&conjunto[i].valor);
+    if (valor_subconjunto > melhor_valor || (valor_subconjunto == melhor_valor && peso_subconjunto < melhor_peso)) {
+        melhor_valor = valor_subconjunto;
+        melhor_peso = peso_subconjunto;
+        tamanho_melhor_subconjunto = tamanho_subconjunto;
+        for (int i = 0; i < tamanho_subconjunto; i++) {
+            melhores_indices[i] = indices_subconjunto[i];
+        }
     }
 
-    printf("%d\n", f(quant_conjuntos,peso_max,conjunto));
-    
+    for (int i = 0; i < n; i++) {
+        if (!(mascara & (1 << i))) {
+            indices_subconjunto[tamanho_subconjunto] = S[i].indice;
+            encontrar_subconjunto(S, n, M, mascara | (1 << i), peso_subconjunto + S[i].peso, valor_subconjunto + S[i].valor, tamanho_subconjunto + 1, indices_subconjunto);
+        }
+    }
+}
+
+int main() {
+    int n, M;
+    Objeto S[MAX_N];
+
+    scanf("%d %d", &n, &M);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d %d", &S[i].peso, &S[i].valor);
+        S[i].indice = i;
+    }
+
+    int indices_subconjunto[MAX_N] = {0};
+    encontrar_subconjunto(S, n, M, 0, 0, 0, 0, indices_subconjunto);
+
+    printf("%d %d\n", melhor_peso, melhor_valor);
+    printf("%d", tamanho_melhor_subconjunto);
+    for (int i = 0; i < tamanho_melhor_subconjunto; i++) {
+        printf(" %d", melhores_indices[i]);
+    }
+    printf("\n");
+
     return 0;
 }
-int max(int a , int b){
-    return (a > b ? a : b);
-}
-int f(int quant_conjuntos, int peso_max, item conjunto[]){
-    if(quant_conjuntos == 0 || peso_max == 0) return 0;
-
-    if (conjunto[quant_conjuntos-1].peso > peso_max) return f(quant_conjuntos-1, peso_max, conjunto);
-    else return max((conjunto[quant_conjuntos-1]. valor + f(quant_conjuntos-1,peso_max - conjunto[quant_conjuntos-1].peso,conjunto)),
-        f(quant_conjuntos-1,peso_max,conjunto));
-}
-
-
-/*pega o primeiro e o segundo item do conjunto (sempre trabalhando em duplas) subtrai o valor do peso
-maximo e se possivel somar com os outros itens do conjunto ate ser <= 100, se for guarda numa variavel
-maximo, logo em seguida pega o segundo e o terceiro e faz o mesmo, depois o terceiro e o quarto e assim
-por diante*/
