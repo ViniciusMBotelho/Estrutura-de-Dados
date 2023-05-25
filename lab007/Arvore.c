a23fee65c8307f2d106d3fdc53498ec84d924b5e
Vinicius Macedo <viniciusmcode@gmail.com>
2023-05-25 15:01:23 -0300

    Finishing lab007

--- lab007/Arvore.c
+++ lab007/Arvore.c
@@ -19,3 +19,35 @@ void imprime(Arvore arv) {
   printf("\n");
 }
 
+bool insere(Arvore* ap_arv, int x){
+  if(*ap_arv == NULL){
+    No* novo = (No*) malloc(sizeof(No));
+    if(novo == NULL) return false;
+
+    novo->dir = NULL;
+    novo->esq = NULL;
+    novo->valor = x;
+    *ap_arv = novo;
+    return true;
+  }
+
+  if(x < (*ap_arv)->valor) return insere(&((*ap_arv)->esq),x);
+  else return insere(&((*ap_arv)->dir),x);
+
+  return false;
+}
+
+bool remove_(Arvore* ap_arv, int x){
+  if(busca(&(*ap_arv),x) == false) return false;
+
+   
+}
+
+bool busca(Arvore arv, int x){
+  if(arv == NULL) return false;
+
+  if(x == arv->valor) return true;
+
+  if(x < arv->valor) return busca(arv->esq,x);
+  else return busca(arv->esq,x);
+}
--- lab007/enunciado.md
+++ lab007/enunciado.md
@@ -1,1 +1,3 @@
 Implemente as funções declaradas no arquivo Arvore.h, insere, busca e remove de árvore de busca binária.
+
+Para compilar gcc -g lab08.c Arvore.c
