01101e91051804cbbdcc25da097c776887823021
Vinicius Macedo <viniciusmcode@gmail.com>
2023-05-25 16:31:04 -0300

    Testing functions insere and remove

--- lab007/Arvore.c
+++ lab007/Arvore.c
@@ -32,22 +32,34 @@ bool insere(Arvore* ap_arv, int x){
   }
 
   if(x < (*ap_arv)->valor) return insere(&((*ap_arv)->esq),x);
-  else return insere(&((*ap_arv)->dir),x);
+  else if(x > (*ap_arv)->valor) return insere(&((*ap_arv)->dir),x);
 
   return false;
 }
 
 bool remove_(Arvore* ap_arv, int x){
-  if(busca(&(*ap_arv),x) == false) return false;
+  Arvore *ap_aux = ap_arv;
+  if(busca(*ap_arv,x) == false) return false;
 
-   
+  while(x != (*ap_aux)->valor){
+    if((*ap_aux)->valor < x) (*ap_aux) = (*ap_aux)->esq;
+    else (*ap_arv) = (*ap_aux)->dir;
+  }
+
+  if((*ap_aux)->esq == NULL && (*ap_aux)->dir == NULL){
+    free(ap_aux);
+    ap_aux == NULL;
+  }else if((*ap_aux)->esq == NULL || (*ap_aux)->dir == NULL){
+    (*ap_aux)->esq == NULL ? ((*ap_aux) = (*ap_aux)->dir) : ((*ap_aux) = (*ap_aux)->esq);
+  }else{
+    
+  }
 }
 
 bool busca(Arvore arv, int x){
   if(arv == NULL) return false;
 
   if(x == arv->valor) return true;
-
   if(x < arv->valor) return busca(arv->esq,x);
-  else return busca(arv->esq,x);
+  else if(x > arv->valor) return busca(arv->dir,x);
 }
